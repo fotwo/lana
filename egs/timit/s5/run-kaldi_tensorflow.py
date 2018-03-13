3bb9ea032d8fa882851fb8f4d64c78b6435652b8
Tong <tongsmail@126.com>
2018-03-13 12:05:09 +0800

    a uniformed script for training and testing with command line parameters.

--- egs/timit/s5/run-kaldi_tensorflow.py
+++ egs/timit/s5/run-kaldi_tensorflow.py
@@ -1,21 +1,24 @@
 # Copyright 2016    Vincent Renkens
 #           2016    Gaofeng Cheng
+#           2018    Peking University (author: Tong Fu)
 ###### Attention please######
-## tensorflow version: 0.11.0rc2
+## tensorflow version: 1.0
 ## python version: python2.7
 ## only supporting: 
 ##                reading features(compress = false) made by kaldi; training neural network with tensorflow.
+## 
 ## This version now only support DNN running on one gpu card, further functions need to be implemented.
 ## before running this ,you should 'source path.sh' first
-## if you want to run tensor-kaldi on another dataset, you need to do some work
-## Results(without ivector)         epoch   eval92_bd   eval92 
-## kaldi 2048 relu 6 layer          8       4.22        6.59
-## tensorflow + kaldi(see config)   ~3      4.93        6.95
+## This work is on TIMIT.
+## Results(without ivector)         epoch   dev   test 
+## kaldi 2048 relu 4 layer          
+## tensorflow + kaldi(see config)   
 ##
-from six.moves import configparser
+
 import os
 import sys
-
+import argparse
+from six.moves import configparser
 from pdb import set_trace
 
 sys.path.append("./")
@@ -32,57 +35,196 @@ import libs.tensorflow.kaldi_tensorflow.ark as ark
 import libs.tensorflow.kaldi_tensorflow.kaldiInterface as kaldiInterface
 import libs.tensorflow.kaldi_tensorflow.batchdispenser as batchdispenser
 
-TRAIN_NNET = True           #required
-TEST_NNET = True            #required if the performance of the DNN is tested
 
+# Get args from stdin.
+def get_args():
+    """
+    Get args from stdin.
+    """
+    parser = argparse.ArgumentParser(
+        description="""Training a DNN acoustic model.""",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        conflict_handler='resolve')
+
+    # configuration file.
+    parser.add_argument("--basic-conf-file",type=str,required=True,dest='basic_conf_file')
+
+    # params for precedure.
+    parser.add_argument("--trian-nnet",type=bool,dest='train_nnet',default=True)
+
+    parser.add_argument("--test-nnet",type=bool,dest='test_nnet',default=True)
+
+    # networks hyper-params.
+    parser.add_argument("--context-width",type=int,dest='context_width',
+                        default=5,help="""size of the left and right context window""")
+
+    parser.add_argument("--num-hidden-units",type=int,dest='num_hidden_units',
+                        default=2048,help="""number of neurons in the hidden layers""")
+
+    parser.add_argument("--num-hidden-layers",type=int,dest='num_hidden_layers',
+                        default=4,help="""number of hidden layers""")
+
+    parser.add_argument("--activation",type=str,dest='activation',
+                        default='relu',help="""nonlinearity used currently supported: relu, tanh, sigmoid""")
+
+
+    # training hyper-params.
+    parser.add_argument("--initial-learning-rate",type=float,dest='initial_learning_rate',
+                        default=0.001,help="""initial learning rate of the neural net""")
+
+    parser.add_argument("--learning-rate-decay",type=int,dest="learning_rate_decay",
+                        default=1,help="""exponential weight decay parameter""")
+
+    parser.add_argument("--batch-size",type=int,dest='batch_size',
+                        default=128,help="""size of the minibatch (#utterances).
+                        to limit memory ussage (specifically for GPU) the batch can be devided into even smaller batches.
+                        The gradient will be calculated by averaging the gradients of all these mini-batches.""")
+
+    parser.add_argument("--num-frames-per-batch",type=int,dest='num_frames_per_batch',
+                        default=4096,help="""This value is the size of these mini-batches in number of frames.
+                        For optimal speed this value should be set as high as possible without exeeding the memory.
+                        To use the entire batch set to -1""")
+
+    parser.add_argument("--add-layer-period",type=int,dest='add_layer_period',
+                        default=10,help="""the network is initialized layer by layer. This parameters determines the frequency of adding layers.
+                        Adding will stop when the total number of layers is reached.
+                        Set to 0 if no layer-wise initialisation is required.""")
+
+    parser.add_argument("--starting-step",type=int,dest='starting_step',
+                        default=0,help="""starting step, set to 'final' to skip nnet training""")
+
+    parser.add_argument("--monophone",type=bool,dest='monophone',
+                        default=False,help="""if you're using monophone alignments, set to True""")
+
+    parser.add_argument("--num-epochs",type=int,dest='num_epochs',
+                        default=10,help="""number of passes over the entire database""")
+
+    # validation.
+    parser.add_argument("--valid-batches",type=int,dest='valid_batches',
+                        default=2,help="""size of the validation set,
+                        set to 0 if you don't want to use one""")
+
+    parser.add_argument("--valid-frequency",type=int,dest='valid_frequency',
+                        default=10,help="""frequency of evaluating the validation set""")
+
+    parser.add_argument("--valid-adapt",type=bool,dest='valid_adapt',
+                        default=True,help="""if you want to adapt the learning rate based on the validation set, set to True""")
+
+    parser.add_argument("--valid-retries",type=int,dest='valid_retries',
+                        default=3,help="""number of times the learning will retry
+                        (with half learning rate) before terminating the training""")
+
+    # check
+    parser.add_argument("--check-frequency",type=int,dest='check_frequency',
+                        default=10,help="""how many steps are taken between two checkpoints""")
+
+    # visualization.
+    parser.add_argument("--visualize",type=bool,dest='visualize',
+                        default=True,help="""you can visualise the progress of the neural net with tensorboard""")
+
+    # regularization.
+    parser.add_argument("--l2-norm",type=bool,dest='l2_norm',
+                        default=False,help="""if you want to do l2 normalization
+                        after every layer set to 'True'""")
+
+    parser.add_argument("--dropout",type=float,dest='dropout',
+                        default=1.0,help="""if you want to use dropout
+                         set to a value smaller than 1""")
+
+    parser.add_argument("--batch-norm",type=bool,dest='batch_norm',
+                        default=False,help="""Flag for using batch normalisation""")
+
+    # output directory
+    parser.add_argument("--model-name",type=str,dest='model_name',
+                        required=True,help="""name of the neural net""")
+
+    print(" ".join(sys.argv))
+    print(sys.argv)
+
+    args=parser.parse_args()
+
+    return args
+
+args=get_args()
+print(args)
+# set_trace()
 # read config file
 config = configparser.ConfigParser()
 # config.read('conf/wsj_tensorflow_kaldi.conf')
-config.read('conf/timit_tensorflow_kaldi.conf')
+config.read(args.basic_conf_file)
 current_dir = os.getcwd()
 
 # setting setting gpu used
 os.environ['CUDA_VISIBLE_DEVICES']=config.get('general','gpu_card_id')
 
 # get the feature input dim
-reader = ark.ArkReader(config.get('directories','train_features') + '/' + config.get('dnn-features','name') + '/feats.scp')
+reader = ark.ArkReader(config.get('directories','train_features') + '/' + config.get('dnn-features','train') + '/feats.scp')
 # print(config.get('directories','train_features') + '/' + config.get('dnn-features','name') + '/feats.scp')
 # set_trace()
 (_,features,_) = reader.read_next_utt()
 input_dim = features.shape[1]
 
 # get number of output labels
-graph_dir_split = config.get('nnet','graph_dir').strip().split(',')
+graph_dir_split = config.get('graph','graph_dir').strip().split(',')
 if len(graph_dir_split) is 0:
     print("Error: there should be lang dir")
     exit()
 #
 
 # here we suppose the pdf num shared by different graph_dir is the same
-gmm_dir = config.get('directories','expdir') + '/' + config.get('nnet','gmm_name')
-gmm_ali_dir = config.get('directories','expdir') + '/' + config.get('nnet','gmm_ali_name')
+gmm_dir = config.get('directories','expdir') + '/' + config.get('gmm','gmm_name')
+gmm_ali_dir = config.get('directories','expdir') + '/' + config.get('gmm','gmm_ali_name')
 numpdfs = open(gmm_dir + '/'+graph_dir_split[0]+'/num_pdfs')
 num_labels = numpdfs.read()
 num_labels = int(num_labels[0:len(num_labels)-1])
 numpdfs.close()
-print("num_labels: "+str(num_labels))    
+print("num_labels: "+str(num_labels))
 print("input_dim: "+str(input_dim))
 
-#create the neural net     
+# For compatible with the original kaldi-tensorflow.
+config.add_section('nnet')
+config.set('nnet','name',args.model_name)
+config.set('nnet','gmm_ali_name',config.get('gmm','gmm_ali_name'))
+config.set('nnet','gmm_name',config.get('gmm','gmm_name'))
+config.set('nnet','graph_dir',config.get('graph','graph_dir'))
+config.set('nnet','context_width',str(args.context_width))
+config.set('nnet','num_hidden_units',str(args.num_hidden_units))
+config.set('nnet','num_hidden_layers',str(args.num_hidden_layers))
+config.set('nnet','add_layer_period',str(args.add_layer_period))
+config.set('nnet','starting_step',str(args.starting_step))
+config.set('nnet','monophone',str(args.monophone))
+config.set('nnet','nonlin',str(args.activation))
+config.set('nnet','l2_norm',str(args.l2_norm))
+config.set('nnet','dropout',str(args.dropout))
+config.set('nnet','batch_norm',str(args.batch_norm))
+config.set('nnet','num_epochs',str(args.num_epochs))
+config.set('nnet','initial_learning_rate',str(args.initial_learning_rate))
+config.set('nnet','learning_rate_decay',str(args.learning_rate_decay))
+config.set('nnet','batch_size',str(args.batch_size))
+config.set('nnet','numframes_per_batch',str(args.num_frames_per_batch))
+config.set('nnet','valid_batches',str(args.valid_batches))
+config.set('nnet','valid_frequency',str(args.valid_frequency))
+config.set('nnet','valid_adapt',str(args.valid_adapt))
+config.set('nnet','valid_retries',str(args.valid_retries))
+config.set('nnet','check_freq',str(args.check_frequency))
+config.set('nnet','visualise',str(args.visualize))
+#
+# set_trace()
+#create the neural net
 Nnet = nnet.Nnet(config, input_dim, num_labels)
 
-if TRAIN_NNET:
+if args.train_nnet:
     #only shuffle if we start with initialisation
     if config.get('nnet','starting_step') == '0':
         #shuffle the examples on disk
         print('------- shuffling examples ----------')
-        kaldiInterface.shuffle_examples(config.get('directories','train_features') + '/' +  config.get('dnn-features','name'))
-        print("train_data_root: "+str(config.get('directories','train_features') + '/' +  config.get('dnn-features','name')))
+        kaldiInterface.shuffle_examples(config.get('directories','train_features') + '/' +  config.get('dnn-features','train'))
+        print("train_data_root: "+str(config.get('directories','train_features') + '/' +  config.get('dnn-features','train')))
     #put all the alignments in one file
     os.system('rm -fr %s %s > /dev/null' % (gmm_ali_dir+'/ali.all.gz', gmm_ali_dir+'/ali.all.pdf.txt.gz'))
     gmm_dir_ali = gmm_ali_dir+'/ali.*.gz'
-    os.system('ls %s | grep [0-9] | wc -l > %s' % (gmm_dir_ali,config.get('general','gmm_ali_number')))
-    f = open(config.get('general','gmm_ali_number'), 'r')
+    os.system('ls %s | grep [0-9] | wc -l > %s' % (gmm_dir_ali,config.get('gmm','gmm_ali_number')))
+    f = open(config.get('gmm','gmm_ali_number'), 'r')
     gmm_ali_jobs_num = f.read()
     f.close()
     if int(gmm_ali_jobs_num) is 0:
@@ -92,7 +234,7 @@ if TRAIN_NNET:
     print(alifiles)
     alifile = gmm_ali_dir + '/ali.all.gz'
     alifile_unzip = gmm_ali_dir + '/ali.all'
-    ali_final_mdl = config.get('directories','expdir') + '/'+ config.get('nnet','gmm_ali_name') + '/final.mdl'
+    ali_final_mdl = config.get('directories','expdir') + '/'+ config.get('gmm','gmm_ali_name') + '/final.mdl'
     alifile_in_pdftxt = gmm_ali_dir + '/ali.all.pdf.txt'
     alifile_in_pdftxt_gzipped = gmm_ali_dir + '/ali.all.pdf.txt.gz'
 
@@ -107,19 +249,19 @@ if TRAIN_NNET:
     os.system('ali-to-pdf %s ark:%s ark,t:%s' % (ali_final_mdl, alifile_unzip, alifile_in_pdftxt)) 
     os.system('gzip -c %s > %s' % (alifile_in_pdftxt, alifile_in_pdftxt_gzipped))
     #train the neural net
-    set_trace()
+    # set_trace()
     print('------- training neural net ----------')
-    Nnet.train(config.get('directories','train_features') + '/' +  config.get('dnn-features','name'), alifile_in_pdftxt_gzipped)
+    Nnet.train(config.get('directories','train_features') + '/' +  config.get('dnn-features','train'), alifile_in_pdftxt_gzipped)
 
-if TEST_NNET:
+if args.test_nnet:
     # set_trace()
     #use the neural net to calculate posteriors for the testing set
     print('------- computing state pseudo-likelihoods ----------')
-    savedir = config.get('directories','expdir') + '/' + config.get('nnet','name')
+    savedir = config.get('directories','expdir') + '/' + args.model_name
     print("pseudo-likelihoods savedir: "+savedir)
 
     # decoding test feature with different graph_dir, of course, test feature dir always more than 1 
-    test_feature_dir = config.get('dnn-features','test_feature_name')
+    test_feature_dir = config.get('dnn-features','test')
     test_feature_dir_split = test_feature_dir.strip().split(',')
     
     # decoding all test feature sets and lang dir
